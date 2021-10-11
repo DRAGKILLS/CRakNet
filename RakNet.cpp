d9ce4f63786c6261c16dc517b432d7d876101626
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-10-11 05:16:31 -0700

    use static

--- RakNet.cpp
+++ RakNet.cpp
@@ -2,11 +2,11 @@
 #include <Windows.h>
 #include <stdlib.h>
 
-int PROTOCOL = 10;
-byte FLAG_NEED_ACK = 0x08;
-int PRIORITY_NORMAL = 0;
-int PRIORITY_IMMEDIATE = 1;
-char MAGIC[] = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78";
+static int PROTOCOL = 10;
+static byte FLAG_NEED_ACK = 0x08;
+static int PRIORITY_NORMAL = 0;
+static int PRIORITY_IMMEDIATE = 1;
+static char MAGIC[] = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78";
 
 int main()
 {
