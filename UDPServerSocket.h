2fccf5b2b4f835ed7c2cdfb5471561e044394178
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-10-12 15:56:00 +0300

    Create UDPServerSocket.h

--- UDPServerSocket.h
+++ UDPServerSocket.h
@@ -6,5 +6,5 @@
 int set_nonblocking_socket(int fd, int nonblocking);
 int create_socket(char* Address, int Port);
 int getSocket();
-int sendBuffer(char buffer, int len, int flags);
+int sendBuffer(char buffer, int length, int flags);
 int close();
