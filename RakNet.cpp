0da402e6103086990c4eedff084003d566478189
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-10-12 01:56:04 -0700

    Update RakNet.cpp

--- RakNet.cpp
+++ RakNet.cpp
@@ -2,18 +2,19 @@
 #include <Windows.h>
 #include <stdlib.h>
 
-char VERSION[] = "1.0";
+char VERISON[] = "1.0";
 static int PROTOCOL = 10;
 static byte FLAG_NEED_ACK = 0x08;
 static int PRIORITY_NORMAL = 0;
 static int PRIORITY_IMMEDIATE = 1;
 static char MAGIC[] = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78";
+static char ADDRESS[] = "0.0.0.0";
+static int PORT = 19132;
 
 using namespace std;
 
 int main()
 {
-    cout << "Starting Host in " << AF_INET << ":19132!" << endl;
-
+    cout << "Starting Host in 0.0.0.0:19132!" << endl;
     return 0;
 }
