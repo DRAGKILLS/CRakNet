da8b058a1e49d187ed104193e7b096ed6b460e8b
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-10-12 01:53:59 -0700

    Update UDPServerSocket.h

--- UDPServerSocket.h
+++ UDPServerSocket.h
@@ -3,6 +3,8 @@
 #include <Windows.h>
 #include <stdlib.h>
 
+int set_nonblocking_socket(int fd, int nonblocking);
+int create_socket(char *Address, int Port);
 int getSocket();
 int sendBuffer(char buffer, int len, int flags);
 int close();
