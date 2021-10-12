7b5255ab02956cd3a3cccb268d8c0d6b45a8c3af
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-10-12 10:52:29 +0300

    use namespace

--- RakNet.cpp
+++ RakNet.cpp
@@ -9,9 +9,11 @@ static int PRIORITY_NORMAL = 0;
 static int PRIORITY_IMMEDIATE = 1;
 static char MAGIC[] = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78";
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Starting Host in " << AF_INET << ":19132!" << std::endl;
+    cout << "Starting Host in " << AF_INET << ":19132!" << endl;
 
     return 0;
 }
